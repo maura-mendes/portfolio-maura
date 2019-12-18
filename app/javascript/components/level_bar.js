f5960d33ab7e8cca7631c73c07beca77dd9c77f0
maurabaptista <maura.mendes.baptista@gmail.com>
2019-12-18 15:13:15 +0100

    added resume, alterations on my interests section on mobile and desk, my resyme btn

--- app/javascript/components/level_bar.js
+++ app/javascript/components/level_bar.js
@@ -1,7 +1,6 @@
 import jquery from "jquery";
 // import {} from 'jquery-ujs'
 
-
 jQuery(document).ready(function($) {
 
 
@@ -10,15 +9,29 @@ jQuery(document).ready(function($) {
     
     $('.level-bar-inner').css('width', '0');
     
-    $(window).on('load', function() {
+    var element_position = $('#my-skills').offset().top;
+
+    var screen_height = $(window).height();
+    var activation_offset = 0.5;//determines how far up the the page the element needs to be before triggering the function
+    var activation_point = element_position - (screen_height * activation_offset);
+    var max_scroll_height = $('body').height() - screen_height - 5;//-5 for a little bit of buffer
+    $(window).on('scroll', function() {
 
         $('.level-bar-inner').each(function() {
         
-            var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
+            var y_scroll_pos = window.pageYOffset;
+            var element_in_view = y_scroll_pos > activation_point;
+            var has_reached_bottom_of_page = max_scroll_height <= y_scroll_pos && !element_in_view;
+
+               if(element_in_view || has_reached_bottom_of_page) {
+                   var itemWidth = $(this).data('level');
+                   
+                   $(this).animate({
+                       width: itemWidth
+                   }, 800);
+               }
+
+          
             
         });
 
@@ -27,4 +40,3 @@ jQuery(document).ready(function($) {
     
 
 });
-
